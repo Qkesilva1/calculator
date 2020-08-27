b363183dfe0d4a58f2f9eb0e6f46bf0b6a4cfddb
Cleiton <Qkesilva1@hotmail.com>
2020-08-27 19:06:58 -0300

    Funcionalidades da calculadora

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
 import React, { Component } from 'react';
-import {Platform, StyleSheet, Text, View, useState, Dimensions } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
-export default class App extends Component {
-  state = {
+const initialState ={
     displayValue: '0',
-  }
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0,
+}
+
+export default class App extends Component {
+    state = {...initialState}
 
   addDigit = n => {
-    this.setState({displayValue: n})
+    const clearDisplay = this.state.displayValue === '0' 
+    || this.state.clearDisplay;
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')){
+      return
+    }
+
+    const currentValue = clearDisplay ? ' ' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({displayValue, clearDisplay: false});
+
+    if(n !== '.'){
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[this.state.current] = newValue;
+      this.setState({ values });
+    }
   }
 
   clearMemory = () => {
-    this.setState({displayValue: '0'})
+    this.setState({...initialState});
   }
 
   setOperation = operation => {
+    if(this.state.current === 0){
+      this.setState({operation, current: 1, clearDisplay: true})
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values]
+      try{
+        values[0] = 
+          eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
+  }
+
+  setContrary = n => {
+    this.setState({displayValue: 0 - n})
   }
 
   render(){
@@ -60,4 +106,3 @@ const style = StyleSheet.create({
     flexWrap: 'wrap',
   },
 });
-
